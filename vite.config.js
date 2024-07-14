688d3b6e3391ef2f4716eb7376d6951032e947b2
iliyanMarkov <ilkomarkov000@gmail.com>
2024-07-14 23:30:38 +0300

    Open app run

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	base: "/iliyan-markov/",
+	server: {
+		open: true,
+		hmr: {
+			protocol: "ws",
+			host: "localhost",
+		},
+	},
 });
